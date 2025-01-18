fcff6ce3712e5ebf9d975f9f1bb99a0627cc0f0d
Mango <evan940415@gmail.com>
2025-01-18 19:04:17 +0800

    fix: navbar order

--- docs/.vuepress/theme/navbar/zhtw.ts
+++ docs/.vuepress/theme/navbar/zhtw.ts
@@ -1,11 +1,11 @@
 import { navbar } from "vuepress-theme-hope";
 
 export const zhtwNavbar = navbar([
-  "/zh-tw/",
   {
     text: "Wiki",
     link: "https://wiki.mango-kubejs.pages.dev/zh-tw/",
   },
+  "/zh-tw/",
   {
     text: "文章",
     link: "https://artcle.mango-kubejs.pages.dev/zh-tw/",
