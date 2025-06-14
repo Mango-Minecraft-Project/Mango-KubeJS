89330aacdd7f6f9841c7fd19f7fadd59edfa7a09
EvanHsieh0415 <evan940415@gmail.com>
2025-06-14 08:19:53 +0800

    feat: new feature

--- docs/.vuepress/client.ts
+++ docs/.vuepress/client.ts
@@ -1,5 +1,6 @@
 import { defineClientConfig } from "vuepress/client";
 import { setupRunningTimeFooter } from "vuepress-theme-hope/presets/footerRunningTime.js";
+import { setupTransparentNavbar } from "vuepress-theme-hope/presets/transparentNavbar.js";
 
 import CustomComponents from "./components/index.js";
 import layouts from "./layouts/index.js";
@@ -11,11 +12,15 @@ export default defineClientConfig({
     Object.entries(CustomComponents).forEach(addComponent);
   },
   setup() {
-    setupRunningTimeFooter(new Date("2025-01-18T02:33:25.000Z"), {
-      "/": "Running time: :day days :hour hours :minute minutes :second seconds",
-      "/zh-tw/": "已運行 :day 天 :hour 小時 :minute 分鐘 :second 秒",
-      "/zh-cn/": "已运行 :day 天 :hour 小时 :minute 分钟 :second 秒",
-    },)
+    setupRunningTimeFooter(
+      new Date("2025-01-18"),
+      {
+        "/": "Running time: :day days :hour hours :minute minutes :second seconds",
+        "/zh-tw/": "已運行 :day 天 :hour 小時 :minute 分鐘 :second 秒",
+        "/zh-cn/": "已运行 :day 天 :hour 小时 :minute 分钟 :second 秒",
+      },
+      true
+    );
   },
   layouts,
 });
